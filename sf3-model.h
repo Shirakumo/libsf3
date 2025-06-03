f60bc20701bbcfd72c8cae6b6c99349aa2a558b9
Yukari Hafner <shinmera@tymoon.eu>
2025-06-03 11:29:17 +0200

    Implement model

--- sf3-model.h
+++ sf3-model.h
@@ -2,6 +2,75 @@
 
 #define SF3_FORMAT_ID_MODEL 0x05
 
+enum sf3_material_type{
+  SF3_MATERIAL_ALBEDO = 0x01,
+  SF3_MATERIAL_NORMAL = 0x02,
+  SF3_MATERIAL_METALLIC = 0x04,
+  SF3_MATERIAL_METALNESS = 0x08,
+  SF3_MATERIAL_ROUGHNESS = 0x10,
+  SF3_MATERIAL_OCCLUSION = 0x20,
+  SF3_MATERIAL_SPECULAR = 0x40,
+  SF3_MATERIAL_EMISSION = 0x80,
+};
+
+enum sf3_vertex_format{
+  SF3_VERTEX_POSITION = 0x01,
+  SF3_VERTEX_UV = 0x02,
+  SF3_VERTEX_COLOR = 0x04,
+  SF3_VERTEX_NORMAL = 0x08,
+  SF3_VERTEX_TANGENT = 0x10,
+};
+
+struct sf3_faces{
+  uint32_t count;
+  uint32_t faces[];
+};
+
+struct sf3_vertices{
+  uint32_t count;
+  float vertices[];
+};
+
 struct sf3_model{
   struct sf3_identifier identifier;
+  uint8_t vertex_format;
+  uint8_t material_type;
+  uint32_t material_size;
+  sf3_str16 textures[];
 };
+
+const sf3_str16 *sf3_model_next_texture(const struct sf3_str16 *texture){
+  return (const sf3_str16 *)SF3_SKIP_STRP(texture);
+}
+
+const struct sf3_faces *sf3_model_faces(const struct sf3_model *model){
+  return (const struct sf3_faces *)(((char*)model->textures)+model->material_size);
+}
+
+const struct sf3_vertices *sf3_model_vertices(const struct sf3_model *model){
+  const struct sf3_faces *faces = sf3_model_faces(model);
+  return (const struct sf3_vertices *)(faces->faces+faces->count);
+}
+
+uint8_t sf3_model_material_count(const struct sf3_model *model){
+  uint8_t type = model->material_type;
+  return
+    (type >> 0) & 1 +
+    (type >> 1) & 1 +
+    (type >> 2) & 1 +
+    (type >> 3) & 1 +
+    (type >> 4) & 1 +
+    (type >> 5) & 1 +
+    (type >> 6) & 1 +
+    (type >> 7) & 1;
+}
+
+uint8_t sf3_model_vertex_stride(const struct sf3_model *model){
+  uint8_t type = model->vertex_format;
+  return
+    ((type >> 0) & 1) * 3 +
+    ((type >> 1) & 1) * 2 +
+    ((type >> 2) & 1) * 3 +
+    ((type >> 3) & 1) * 3 +
+    ((type >> 4) & 1) * 3;
+}
