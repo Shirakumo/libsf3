57ca069894c506f69a904e63d98718451877465d
Yukari Hafner <shinmera@tymoon.eu>
2025-07-02 11:40:45 +0200

    Make the sf3_lib better.

--- src/sf3_lib.c
+++ src/sf3_lib.c
@@ -6,13 +6,52 @@
 #include <sys/stat.h>
 #include "sf3_lib.h"
 
+#ifndef thread_local
+# if defined NN_NINTENDO_SDK
+#  define thread_local
+# elif __STDC_VERSION__ >= 201112 && !defined __STDC_NO_THREADS__
+#  define thread_local _Thread_local
+# elif defined _WIN32 && ( defined _MSC_VER || defined __ICL || defined __DMC__ || defined __BORLANDC__ )
+#  define thread_local __declspec(thread)
+/* note that ICC (linux) and Clang are covered by __GNUC__ */
+# elif defined __GNUC__ || defined __SUNPRO_C || defined __xlC__
+#  define thread_local __thread
+# else
+#  define thread_local
+# endif
+#endif
+
 struct handle{
-  enum SF3_OPEN_MODE mode;
+  enum sf3_open_mode mode;
   int fd;
   size_t size;
   void *addr;
 };
 
+thread_local enum sf3_error err = SF3_OK;
+
+SF3_EXPORT enum sf3_error sf3_error(){
+  return err;
+}
+
+SF3_EXPORT const char *sf3_strerror(enum sf3_error error){
+  switch(error){
+  case SF3_OK:
+    return "No error occurred.";
+  case SF3_OPEN_FAILED:
+    return "Failed to open file.";
+  case SF3_OUT_OF_MEMORY:
+    return "Out of memory.";
+  case SF3_MMAP_FAILED:
+    return "Failed to map file into memory.";
+  case SF3_INVALID_HANDLE:
+    return "Invalid sf3 file handle.";
+  case SF3_WRITE_FAILED:
+    return "Failed to write file.";
+  default:
+    return "Unknown error";
+  }
+}
 
 SF3_EXPORT int sf3_tell(const char *path){
   void *handle;
@@ -26,18 +65,23 @@ SF3_EXPORT int sf3_tell(const char *path){
   return res;
 }
 
-SF3_EXPORT int sf3_open(const char *path, enum SF3_OPEN_MODE mode, sf3_handle *handle){
+SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *handle){
   struct handle *h = (struct handle *)sf3_calloc(1, sizeof(struct handle));
-  if(!h) return -1;
+  if(!h){
+    err = SF3_OUT_OF_MEMORY;
+    return 0;
+  }
 
   struct stat stat;
   h->fd = open(path, (mode)? O_RDWR : O_RDONLY);
   
   if(h->fd == -1){
+    err = SF3_OPEN_FAILED;
     goto cleanup;
   }
   
   if(fstat(h->fd, &stat) == -1){
+    err = SF3_OPEN_FAILED;
     goto cleanup;
   }
 
@@ -48,6 +92,7 @@ SF3_EXPORT int sf3_open(const char *path, enum SF3_OPEN_MODE mode, sf3_handle *h
                  (mode)? MAP_SHARED : MAP_PRIVATE,
                  h->fd, 0);
   if(h->addr == MAP_FAILED){
+    err = SF3_MMAP_FAILED;
     goto cleanup;
   }
 
@@ -82,19 +127,59 @@ SF3_EXPORT void *sf3_data(sf3_handle handle, size_t *size){
     *size = h->size;
     return h->addr;
   }
+  err = SF3_INVALID_HANDLE;
   return 0;
+}
+
+SF3_EXPORT int sf3_create(void *addr, size_t size, sf3_handle *handle){
+  struct handle *h = (struct handle *)sf3_calloc(1, sizeof(struct handle));
+  if(!h){
+    err = SF3_OUT_OF_MEMORY;
+    return -1;
+  }
+  h->mode = SF3_OPEN_READ_WRITE;
+  h->fd = -1;
+  h->size = size;
+  h->addr = addr;
+  *handle = h;
+  return 1;
 }
 
 SF3_EXPORT int sf3_write(const char *path, sf3_handle handle){
   struct handle *h = (struct handle *)handle;
-  if(!path){
-    if(h->mode == SF3_OPEN_READ_WRITE){
+  struct sf3_identifier *id = (struct sf3_identifier *)h->addr;
+  if(h->size < sizeof(struct sf3_identifier)){
+    err = SF3_INVALID_HANDLE;
+    return 0;
+  }else if(!path){
+    if(h->mode == SF3_OPEN_READ_WRITE && h->fd != -1){
+      size_t size = sf3_size(id);
+      sf3_write_header(id->format_id, h->addr, size);
       return msync(h->addr, h->size, MS_SYNC) == 0;
     }else{
       return 0;
     }
   }else{
+    struct sf3_identifier header;
+    const void *payload = ((const uint8_t*)h->addr+sizeof(struct sf3_identifier));
+    size_t size = sf3_size(id);
     
+    int fd = open(path, O_WRONLY | O_CREAT);
+    if(fd == -1){
+      err = SF3_OPEN_FAILED;
+      return 0;
+    }
+    sf3_write_header(id->format_id, &header, size);
+    write(fd, &header, sizeof(struct sf3_identifier));
+    size -= sizeof(struct sf3_identifier);
+    if(write(fd, payload, size) < size){
+      err = SF3_WRITE_FAILED;
+      close(fd);
+      return 0;
+    }
+    ftruncate(fd, size);
+    close(fd);
+    return 1;
   }
 }
 
--- src/sf3_lib.h
+++ src/sf3_lib.h
@@ -29,32 +29,76 @@ extern "C" {
 #include "sf3.h"
 #include<stdio.h>
 
-enum SF3_OPEN_MODE{
-  SF3_OPEN_READ_ONLY = 0,
-  SF3_OPEN_READ_WRITE = 1,
-};
+  enum sf3_open_mode{
+    SF3_OPEN_READ_ONLY = 0,
+    SF3_OPEN_READ_WRITE = 1,
+  };
 
-typedef void *sf3_handle;
+  enum sf3_error{
+    SF3_OK = 0,
+    SF3_OPEN_FAILED,
+    SF3_OUT_OF_MEMORY,
+    SF3_MMAP_FAILED,
+    SF3_INVALID_HANDLE,
+    SF3_WRITE_FAILED,
+  };
 
-/// Returns the sf3_format_id of the file or 0 on failure.
-///
-/// The failure can occur because the path is inaccessible, there is
-/// no more memory available, or the file is not an actual SF3 file.
-/// Note that this will not perform a CRC32 checksum verification of
-/// the file.
-SF3_EXPORT int sf3_tell(const char *path);
+  typedef void *sf3_handle;
 
-/// 
-SF3_EXPORT int sf3_open(const char *path, enum SF3_OPEN_MODE mode, sf3_handle *handle);
+  /// Return the error code.
+  /// This is thread local.
+  SF3_EXPORT enum sf3_error sf3_error();
 
-/// 
-SF3_EXPORT void sf3_close(sf3_handle handle);
+  /// Return a string representation of the error code.
+  SF3_EXPORT const char *sf3_strerror(enum sf3_error error);
 
-/// 
-SF3_EXPORT void *sf3_data(sf3_handle handle, size_t *size);
+  /// Returns the sf3_format_id of the file or 0 on failure.
+  ///
+  /// The failure can occur because the path is inaccessible, there is
+  /// no more memory available, or the file is not an actual SF3 file.
+  /// Note that this will not perform a CRC32 checksum verification of
+  /// the file.
+  SF3_EXPORT int sf3_tell(const char *path);
 
-/// 
-SF3_EXPORT int sf3_write(const char *path, sf3_handle handle);
+  /// Opens the SF3 file at the given path.
+  /// 
+  /// If successful returns a positive value and stores the handle in
+  /// the handle argument. You can then use sf3_data to retrieve the
+  /// actual sf3 file address and size.
+  SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *handle);
+
+  /// Closes the file handle.
+  ///
+  /// After closing the handle is discarded and calling any function
+  /// with it leads to undefined behaviour.
+  SF3_EXPORT void sf3_close(sf3_handle handle);
+
+  /// Returns the address of the SF3 file payload and its size.
+  /// 
+  /// This may fail if the handle is invalid, in which case a null
+  /// pointer is returned.
+  SF3_EXPORT void *sf3_data(sf3_handle handle, size_t *size);
+
+  /// Create a handle from a given SF3 file payload in memory.
+  ///
+  /// This may be useful if you want to call sf3_write to serialize
+  /// the SF3 file back out to a file.
+  SF3_EXPORT int sf3_create(void *addr, size_t size, sf3_handle *handle);
+
+  /// Write the SF3 file back out to a file.
+  ///
+  /// PATH may be a null pointer if the handle was obtained through
+  /// sf3_open with mode set to SF3_OPEN_READ_WRITE. In that case this
+  /// will write the file header back into memory with a freshly
+  /// computed CRC32 checksum, and then call msync to flush the file.
+  ///
+  /// If the PATH is not a null pointer, the SF3 file is written out
+  /// to the file at that path. As part of this the SF3 header is
+  /// written fresh, including computing a fresh CRC32 checksum. The
+  /// memory of the file is **not** modified, however.
+  /// If the file at PATH already exists, it is truncated to the size
+  /// of the SF3 file after writing.
+  SF3_EXPORT int sf3_write(const char *path, sf3_handle handle);
 
 #ifdef SF3_NO_CUSTOM_ALLOCATOR
 #define sf3_calloc calloc
