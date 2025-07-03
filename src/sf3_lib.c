3aa369aa96a022e077ac37c5242c46c72646a823
Yukari Hafner <shinmera@tymoon.eu>
2025-07-03 09:53:48 +0200

    Try and make libsf3 work on windows, conditionalise out based on availability of mman.h and stat.h

--- src/sf3_lib.c
+++ src/sf3_lib.c
@@ -3,8 +3,14 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#if defined(_WIN32)
+#include <windows.h>
+#elif defined(HAVE_MMAN_H)
 #include <sys/mman.h>
+#endif
+#if defined(HAVE_STAT_H)
 #include <sys/stat.h>
+#endif
 #include "sf3_lib.h"
 
 #ifndef thread_local
@@ -24,7 +30,12 @@
 
 struct handle{
   enum sf3_open_mode mode;
+#if defined(_WIN32)
+  HANDLE handle;
+  HANDLE fd;
+#else
   int fd;
+#endif
   size_t size;
   void *addr;
 };
@@ -69,6 +80,25 @@ SF3_EXPORT int sf3_tell(const char *path){
   return res;
 }
 
+static ssize_t file_size(int fd){
+#if defined(HAVE_STAT_H)
+  struct stat stat;
+  if(fstat(fd, &stat) == -1){
+    return -1;
+  }else{
+    return stat.st_size;
+  }
+#else
+  off_t end = lseek(fd, 0, SEEK_END);
+  if(end == (off_t) -1){
+    return -1;
+  }else{
+    lseek(fd, 0, SEEK_SET);
+    return end;
+  }
+#endif
+}
+
 SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *handle){
   err = SF3_OK;
   struct handle *h = (struct handle *)sf3_calloc(1, sizeof(struct handle));
@@ -77,7 +107,34 @@ SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *h
     return 0;
   }
 
-  struct stat stat;
+#if defined(_WIN32)
+  h->fd = CreateFile(path,
+                     ((mode)? GENERIC_WRITE : 0) | GENERIC_READ,
+                     FILE_SHARE_DELETE | FILE_SHARE_READ | FILE_SHARE_WRITE,
+                     NULL, OPEN_EXISTING, 0, NULL);
+
+  if(h->fd == INVALID_HANDLE_VALUE){
+    err = SF3_OPEN_FAILED;
+    goto cleanup;
+  }
+
+  LARGE_INTEGER size;
+  if(!GetFileSizeEx(h->fd, &size)){
+    err = SF3_OPEN_FAILED;
+    goto cleanup;
+  }
+
+  h->mode = mode;
+  h->size = size.QuadPart;
+  h->handle = CreateFileMapping(h->fd, NULL, (mode)? PAGE_READWRITE : PAGE_READONLY,
+                                h->size >> 32, h->size, NULL);
+  h->addr = MapViewOfFile(h->handle, (mode)? FILE_MAP_WRITE : FILE_MAP_READ,
+                          0, 0, h->size);
+  if(!h->addr){
+    err = SF3_MMAP_FAILED;
+    goto cleanup;
+  }
+#elif defined(HAVE_MMAN_H)
   h->fd = open(path, (mode)? O_RDWR : O_RDONLY);
   
   if(h->fd == -1){
@@ -85,13 +142,14 @@ SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *h
     goto cleanup;
   }
   
-  if(fstat(h->fd, &stat) == -1){
+  ssize_t size = file_size(h->fd);
+  if(size < 0){
     err = SF3_OPEN_FAILED;
     goto cleanup;
   }
 
   h->mode = mode;
-  h->size = stat.st_size;
+  h->size = size;
   h->addr = mmap(NULL, h->size,
                  (mode)? (PROT_READ | PROT_WRITE) : PROT_READ,
                  (mode)? MAP_SHARED : MAP_PRIVATE,
@@ -100,6 +158,32 @@ SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *h
     err = SF3_MMAP_FAILED;
     goto cleanup;
   }
+#else
+  h->fd = open(path, (mode)? O_RDWR : O_RDONLY);
+  
+  if(h->fd == -1){
+    err = SF3_OPEN_FAILED;
+    goto cleanup;
+  }
+
+  ssize_t size = file_size(h->fd);
+  if(size < 0){
+    err = SF3_OPEN_FAILED;
+    goto cleanup;
+  }
+
+  h->mode = mode;
+  h->size = size;
+  h->addr = sf3_calloc(1, h->size);
+  if(h->addr == NULL){
+    err = SF3_MMAP_FAILED;
+    goto cleanup;
+  }
+  if(read(h->fd, h->addr, h->size) < h->size){
+    err = SF3_MMAP_FAILED;
+    goto cleanup;
+  }
+#endif
 
   int type = sf3_check(h->addr, h->size);
   if(!type){
@@ -118,14 +202,38 @@ SF3_EXPORT int sf3_open(const char *path, enum sf3_open_mode mode, sf3_handle *h
 SF3_EXPORT void sf3_close(sf3_handle handle){
   struct handle *h = (struct handle *)handle;
   if(h){
+#if defined(_WIN32)
+    if(h->addr != NULL){
+      UnmapViewOfFile(h->addr);
+    }
+    if(h->handle != INVALID_HANDLE_VALUE){
+      CloseHandle(h->handle);
+    }
+    if(h->fd != INVALID_HANDLE_VALUE){
+      CloseHandle(h->fd);
+    }
+    h->handle = INVALID_HANDLE_VALUE;
+    h->fd = INVALID_HANDLE_VALUE;
+    h->addr = NULL;
+#elif defined(HAVE_MMAN_H)
     if(h->addr != MAP_FAILED){
       munmap(h->addr, h->size);
-      h->addr = 0;
     }
     if(0 <= h->fd){
       close(h->fd);
-      h->fd = -1;
     }
+    h->fd = -1;
+    h->addr = MAP_FAILED;
+#else
+    if(h->addr){
+      sf3_free(h->addr);
+    }
+    if(0 <= h->fd){
+      close(h->fd);
+    }
+    h->fd = -1;
+    h->addr = NULL;
+#endif
     h->mode = 0;
     h->size = 0;
     sf3_free(h);
@@ -151,7 +259,12 @@ SF3_EXPORT int sf3_create(void *addr, size_t size, sf3_handle *handle){
     return -1;
   }
   h->mode = SF3_OPEN_READ_WRITE;
+#if defined(_WIN32)
+  h->fd = NULL;
+  h->handle = NULL;
+#else
   h->fd = -1;
+#endif
   h->size = size;
   h->addr = addr;
   *handle = h;
@@ -166,10 +279,32 @@ SF3_EXPORT int sf3_write(const char *path, sf3_handle handle){
     err = SF3_INVALID_HANDLE;
     return 0;
   }else if(!path){
+#if defined(_WIN32)
+    if(h->mode == SF3_OPEN_READ_WRITE && h->fd != NULL){
+      size_t size = sf3_size(id);
+      sf3_write_header(id->format_id, h->addr, size);
+      FlushViewOfFile(h->addr, h->size);
+      return FlushFileBuffers(h->fd);
+#elif defined(HAVE_MMAN_H)
     if(h->mode == SF3_OPEN_READ_WRITE && h->fd != -1){
       size_t size = sf3_size(id);
       sf3_write_header(id->format_id, h->addr, size);
       return msync(h->addr, h->size, MS_SYNC) == 0;
+#else
+    if(h->mode == SF3_OPEN_READ_WRITE && h->fd != -1){
+      size_t size = sf3_size(id);
+      sf3_write_header(id->format_id, h->addr, size);
+      if(lseek(h->fd, 0, SEEK_SET) == (off_t) -1){
+        err = SF3_WRITE_FAILED;
+        return 0;
+      }
+      if(write(h->fd, h->addr, size) < size){
+        err = SF3_WRITE_FAILED;
+        return 0;
+      }
+      fsync(h->fd);
+      return 1;
+#endif
     }else{
       return 0;
     }
